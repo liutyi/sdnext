f0103334106aa5bc8c4c825e9715a5290045aae7
Oleksandr Liutyi <liutyi@gmail.com>
2026-04-13 13:53:34 +0000

    ipex 2.7.10+xpu instead of pytorch 2.10.0

--- installer.py
+++ installer.py
@@ -695,11 +695,9 @@ def install_ipex():
     t_start = time.time()
     args.use_ipex = True # pylint: disable=attribute-defined-outside-init
     log.info('IPEX: Intel OneAPI toolkit detected')
-    if args.use_nightly:
-        torch_command = os.environ.get('TORCH_COMMAND', '--upgrade --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/xpu')
-    else:
-        torch_command = os.environ.get('TORCH_COMMAND', 'torch==2.11.0+xpu torchvision==0.26.0+xpu --index-url https://download.pytorch.org/whl/xpu')
-
+    torch_command = os.environ.get('TORCH_COMMAND', 'torch==2.7.1+xpu torchvision==0.22.1+xpu torchaudio==2.7.1+xpu --index-url https://download.pytorch.org/whl/xpu')
+    install(os.environ.get('TORCH_COMMAND', 'torch==2.7.1+xpu torchvision==0.22.1+xpu torchaudio==2.7.1+xpu --index-url https://download.pytorch.org/whl/xpu'), ignore=True)
+    install(os.environ.get('TORCH_COMMAND', 'intel-extension-for-pytorch==2.7.10+xpu oneccl_bind_pt==2.7.0+xpu --extra-index-url https://pytorch-extension.intel.com/release-whl/stable/xpu/us/'), ignore=True)
     ts('ipex', t_start)
     return torch_command
 
